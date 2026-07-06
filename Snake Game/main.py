5334ce2144df9efc9c4417c0479774eb10c2ead2
Krushal Kalkani <krushal.kalkani78@gmail.com>
2026-07-06 15:37:21 -0400

    Add main game logic and snake class implementation for Snake Game

---
+++ Snake Game/main.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+# segment_1 = Turtle("square")
+# segment_1.color("white")
+
+# segment_2 = Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20, 0)
+
+# segment_3 = Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40, 0)
+
+screen.exitonclick()
