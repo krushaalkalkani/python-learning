8695ce3816a0825a7c966c2cf235f0fe49a0ba83
Krushal Kalkani <krushal.kalkani78@gmail.com>
2026-07-10 11:57:40 -0400

    Refactor Snake Game: implement reset functionality for scoreboard and snake, enhancing game restart logic

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -35,8 +35,8 @@ while game_is_on:
 
     # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
     # detect collision with tail
     # for segment in snake.segments:
@@ -48,8 +48,7 @@ while game_is_on:
 
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
 
 
 # segment_2 = Turtle("square")
--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -7,6 +7,9 @@ class scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0 
+        with open("Snake Game/data.txt", "r") as file:
+            self.high_score = int(file.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,8 +18,16 @@ class scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALLIGNMENT,
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALLIGNMENT,
                    font=FONT)
+        
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+        with open("Snake Game/data.txt", "w") as file:
+            file.write(str(self.high_score))
 
     def game_over(self):
         self.goto(0, 0)
